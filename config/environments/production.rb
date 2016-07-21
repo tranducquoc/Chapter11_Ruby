24881f1364b8007349b11da804f6dd54665d105b
tranducquoc <ducquocb2@gmail.com>
2016-07-21 10:03:46 +0700

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
